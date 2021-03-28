f1bd7d0f83ccbd8d198d223fcd548a3c3e42326e
Luther <luther.bell17@stjohns.edu>
2021-03-28 11:18:42 -0400

    Vigenere Cipher

---
+++ vigenere_cipher.py
@@ -0,0 +1,58 @@
+
+alpha = {
+    "a" : 1, "b" : 2, "c" : 3, "d" : 4, "e" : 5, "f" : 6, "g" : 7, "h" : 8, "i" : 9, "j" : 10,
+    "k" : 11, "l" : 12, "m" : 13, "n" : 14, "o" : 15, "p" : 16, "q" : 17, "r" : 18, "s" : 19, 
+    "t" : 20, "u" : 21, "v" : 22,  "w" : 23, "x" : 24, "y" : 25, "z" : 26, " " : 0
+}
+
+reverse_alpha = {
+    1 : "a", 2 : "b", 3 : "c", 4 : "d", 5 : "e", 6 : "f", 7 : "g", 8 : "h", 9 : "i", 10 : "j",
+    11 : "k", 12 : "l", 13 : "m", 14 : "n", 15 : "o", 16 : "p", 17 : "q", 18 : "r", 19 : "s",
+    20 : "t", 21 : "u", 22 : "v", 23 : "w", 24 : "x", 25 : "y", 26 : "z", 0 : " "
+}
+
+def encryption(text, key):
+    message = ""
+    e = []
+    for i in range(0, len(text), len(key)):
+        e.append(text[i : i + len(key)])
+    for m in e:
+        i = 0
+        for j in m:
+            n = (alpha[j] + alpha[key[i]]) % len(alpha.values())
+            message += reverse_alpha[n]
+            i += 1
+    return message
+
+def decryption(text, key):
+    message = ""
+    e = []
+    for i in range(0, len(text), len(key)):
+        e.append(text[i : i + len(key)])
+    for m in e:
+        i = 0
+        for j in m:
+            n = (alpha[j] - alpha[key[i]]) % len(alpha.values())
+            message += reverse_alpha[n]
+            i += 1
+    return message
+
+
+text = "eat bacon"
+key = "random"
+encrypted = encryption(text, key)
+decrypted = decryption(encrypted, key)
+
+print("Encryption:"+encrypted)
+print("Decryption:"+decrypted)
+        
+    
+    
+
+
+
+
+
+
+    
+
